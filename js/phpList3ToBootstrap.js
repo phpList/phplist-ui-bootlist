8c2aff2ebc0aa1c4e128548c55e27c36582f708b
mariez <marielazarate@gmail.com>
2016-03-12 18:14:45 -0300

    optimize js

--- js/phpList3ToBootstrap.js
+++ js/phpList3ToBootstrap.js
@@ -6,9 +6,7 @@ $(document).ready(function() {
     $('#menuTop ul li ul').parent().addClass('dropdown');
     $('#menuTop ul li ul').addClass('dropdown-menu');
     $('#menuTop ul li ul').parent().find('a:first').addClass('dropdown-toggle');
-    $('#menuTop ul li ul').parent().find('a:first').attr('data-toggle','dropdown');
-    $('#menuTop ul li ul').parent().find('a:first').attr('aria-haspopup','true');
-    $('#menuTop ul li ul').parent().find('a:first').attr('aria-expanded','false');
+    $('#menuTop ul li ul').parent().find('a:first').attr({ 'data-toggle':'dropdown','aria-haspopup':'true','aria-expanded':'false' });
 
 /* BUTTONS */
     $('.submit').addClass('btn btn-primary');
@@ -26,29 +24,20 @@ $(document).ready(function() {
 
 /* COLLAPSIBLE */
     $('.accordion').addClass('panel-group');
-    $('.accordion').attr('aria-multiselectable','true');
-    $('.accordion').attr('id','accordion');
-    $('.accordion').attr('role','tablist');
+    $('.accordion').attr({ 'aria-multiselectable':'true', 'id':'accordion','role':'tablist' });
     $('.accordion h3').addClass('panel-title');
     $('.accordion h3').each(function(){ $(this).next('div').andSelf().wrapAll('<div class="panel panel-default"/>'); });
     $('.accordion h3 a').addClass('collapsed');
-    $('.accordion h3 a').attr('data-toggle','collapse');
-    $('.accordion h3 a').attr('aria-controls',function(i) { return 'collapse'+(i+1); });
-    $('.accordion h3 a').attr('aria-expanded','false');
-    $('.accordion h3 a').attr('href', function(i) { return '#collapse'+(i+1); });
-    $('.accordion h3 a').attr('data-parent','#accordion');
-    $('.accordion h3 a').attr('role','button');
+    $('.accordion h3 a').attr({ "role":"button", "data-toggle":"collapse", "data-parent":"#accordion", "aria-expanded":"false",
+        "aria-controls":function(i) { return 'collapse'+(i+1); }, "href":function(i) { return '#collapse'+(i+1);} });
     $('.accordion .panel-default:first h3 a').attr('aria-expanded','true');
     $('.accordion .panel-default div').not('.accordion .panel-default div div').addClass('panel-collapse collapse');
     $('.accordion .panel-default:first .panel-collapse').addClass(' in ');
     $('.accordion .panel-default .panel-collapse').wrapInner('<div class="panel-body"/>');
-    $('.accordion .panel-default .panel-collapse').attr('id', function(i) { return 'collapse'+(i+1); });
-    $('.accordion .panel-default .panel-collapse').attr('aria-labelledby',function(i) { return 'heading'+(i+1); });
-    $('.accordion .panel-default .panel-collapse').attr('role','tabpanel');
+    $('.accordion .panel-default .panel-collapse').attr({ 'role':'tabpanel', 'id':function(i) { return 'collapse'+(i+1); },
+        'aria-labelledby':function(i) { return 'heading'+(i+1); } });
     $('.accordion h3').wrap('<div class="panel-heading"/>');
-    $('.accordion .panel-heading').attr('id', function(i) { return 'heading'+(i+1); });
-    $('.accordion .panel-heading').attr('role','tab');
-
+    $('.accordion .panel-heading').attr({ 'id':function(i) { return 'heading'+(i+1);}, 'role':'tab' });
 
 /* ALERTS */
     $('div.note').addClass('alert alert-warning');
