5c1cf1042d07165816349594e44c0e8f3fa5d57c
mariez <marielazarate@gmail.com>
2016-12-05 01:25:12 -0300

    add third level item support to submenu, add icons to first level items menu, add plugins items submenu support, remove dashboard from "info" submenu, add dashboard at top of menu, fix develop submenu bug.

--- mainmenu.php
+++ mainmenu.php
@@ -5,4 +5,29 @@ include dirname(__FILE__).'/functions.php';
 
 /* fix sections not opening submenues on first click */
 $GLOBALS['pagecategories']['statistics']['toplink'] = 'statsoverview';
- array_push($GLOBALS['pagecategories']['system']['menulinks'],'system');
+$GLOBALS['pagecategories']['develop']['toplink'] = 'tests';
+if ( !in_array('system', $GLOBALS['pagecategories']['system']['menulinks']) ){
+	array_push($GLOBALS['pagecategories']['system']['menulinks'],'system');
+}
+if ( !in_array('editlist', $GLOBALS['pagecategories']['subscribers']['pages']) ){
+	array_push($GLOBALS['pagecategories']['subscribers']['pages'],'editlist');
+}
+
+
+
+/* remove dashboard from info submenu */
+if ( in_array('home', $GLOBALS['pagecategories']['info']['menulinks']) ){
+		$dashk= array_search('home',$GLOBALS['pagecategories']['info']['menulinks']);
+	unset($GLOBALS['pagecategories']['info']['menulinks'][$dashk]);
+	unset($GLOBALS['pagecategories']['info']['pages'][$dashk]);
+}
+/* add dashboard to top */
+if ( !isset($GLOBALS['pagecategories']['home']) && !isset($GLOBALS['pagecategories']['dashboard']) ){
+	$pcrev = array_reverse($GLOBALS['pagecategories']);
+	$pcrev['dashboard'] = array(
+        'toplink' => 'dashboard',
+        'pages' => array('dashboard'),
+        'menulinks' => array(),
+    );
+    $GLOBALS['pagecategories'] = array_reverse($pcrev);
+}
