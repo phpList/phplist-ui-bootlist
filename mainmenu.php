d837c8ac69bbeafc0069c84cf0606c3dbda058a9
mariez <marielazarate@gmail.com>
2016-12-21 21:10:17 -0300

    change include to include_once

--- mainmenu.php
+++ mainmenu.php
@@ -1,7 +1,7 @@
 <?php
 
 // functions for theme bootstrap
-include dirname(__FILE__).'/functions.php';
+include_once dirname(__FILE__).'/functions.php';
 
 /* fix sections not opening submenues on first click */
 $GLOBALS['pagecategories']['statistics']['toplink'] = 'statsoverview';
