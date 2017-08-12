9473d321fdc50979999b9e58b67d4a02c415600c
mariez <marielazarate@gmail.com>
2017-08-12 20:26:41 -0300

    remove scripts not used by Trevelin theme

--- js/phpList3ToBootstrap.js
+++ js/phpList3ToBootstrap.js
@@ -369,13 +369,6 @@ $('#stopqueue').click(function(){
 
 $(document).ready(function(){
 
-	/*not apply myfunction to acccount pages, just add necessary styles*/
-	if ( $('body').hasClass('accsettings') || $('body').hasClass('accinfo') || $('body').hasClass('acchelp')  ){
-		$('body').removeClass('invisible');
-	    $('#languageswitcher select').addClass('selectpicker').attr({ 'data-style':'btn-primary' });
-	    $('.upgradebutton').addClass('btn btn-primary');
-	}
-
 	/* vertical scrolling */
 	if ( $('body').hasClass('configure') ){
 		var id = location.hash;
