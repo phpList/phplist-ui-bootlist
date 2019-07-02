cba6d735c2ca61bac5ed04a4d169c513e9e2c1cf
Sam Tuke <mail@samtuke.com>
2019-07-02 13:07:50 +0200

    Add newlines to concatenated scripts for readability

--- gruntfile.js
+++ gruntfile.js
@@ -53,5 +53,10 @@ module.exports = function(grunt) {
   });
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('mydefault', ['less', 'watch','concat','uglify']);
+	grunt.registerTask('mydefault', [
+        'less'
+        , 'watch'
+        ,'concat'
+        ,'uglify'
+    ]);
 };
