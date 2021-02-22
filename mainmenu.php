425e76706e96ab1bcdd377fed5caf2bf041b8738
Duncan Cameron <phplist@dcameron.me.uk>
2021-02-22 12:11:07 +0000

    Avoid php notices about undefined variable and undefined index, and warning about null parameter.

--- mainmenu.php
+++ mainmenu.php
@@ -3,22 +3,22 @@
 // functions for theme bootstrap
 include_once dirname(__FILE__).'/functions.php';
 
-if ($page_title == 'userclicks') $page_title = s('Click Statistics'); /* REMOVE AFTER RESOLVE MANTIS #18945 */
-
 /* fix sections not opening submenues on first click */
 $GLOBALS['pagecategories']['statistics']['toplink'] = 'statsoverview';
-$GLOBALS['pagecategories']['develop']['toplink'] = 'tests';
+if (isset($GLOBALS['pagecategories']['develop'])) {
+    $GLOBALS['pagecategories']['develop']['toplink'] = 'tests';
+}
 if ( !in_array('system', $GLOBALS['pagecategories']['system']['menulinks']) ){
-	array_push($GLOBALS['pagecategories']['system']['menulinks'],'system');
+    array_push($GLOBALS['pagecategories']['system']['menulinks'],'system');
 }
 if ( !in_array('editlist', $GLOBALS['pagecategories']['subscribers']['pages']) ){
-	array_push($GLOBALS['pagecategories']['subscribers']['pages'],'editlist');
+    array_push($GLOBALS['pagecategories']['subscribers']['pages'],'editlist');
 }
 
 /* add dashboard to top */
 if ( !isset($GLOBALS['pagecategories']['home']) && !isset($GLOBALS['pagecategories']['dashboard']) ){
-	$pcrev = array_reverse($GLOBALS['pagecategories']);
-	$pcrev['dashboard'] = array(
+    $pcrev = array_reverse($GLOBALS['pagecategories']);
+    $pcrev['dashboard'] = array(
         'toplink' => 'dashboard',
         'pages' => array('dashboard'),
         'menulinks' => array(),
